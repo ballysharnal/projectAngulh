3757d08d476223a44ab7748cb11b3ce79bee7408
Triple <sebastien.andrys@gmail.com>
2019-03-27 09:36:09 +0100

    quest 2 for Angular done

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  email = 'http://eandry.fr';
+  age = 8
+  name = 'hodor'
 
   constructor() { }
+
   ngOnInit() {
   }
-  email = 'http://eandry.fr';
-
+  handleButton(event){
+    console.log(event)
+  }
 }
