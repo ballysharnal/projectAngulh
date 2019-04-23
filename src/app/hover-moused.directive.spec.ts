7a5985cdeeeeb5117fe93a91428a0ca2828ed9f2
Triple <sebastien.andrys@gmail.com>
2019-04-23 10:21:35 +0200

    first deployment

--- src/app/hover-moused.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HoverMousedDirective } from './hover-moused.directive';
-
-describe('HoverMousedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverMousedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
