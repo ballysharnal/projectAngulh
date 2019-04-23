46bdc710c61f1d453db16501262c33466756e231
Triple <sebastien.andrys@gmail.com>
2019-04-23 11:08:14 +0200

    Auto-generated commit

--- src/app/is-movie-ok.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsMovieOkDirective } from './is-movie-ok.directive';
-
-describe('IsMovieOkDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IsMovieOkDirective();
-    expect(directive).toBeTruthy();
-  });
-});
